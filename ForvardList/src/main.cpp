14b95cb5c31352eaa2aa2bfd79a82a57d54cf1cc
Roman <romanavev@gmal.com>
2022-01-31 22:21:57 +0300

    added methods inser and deletinc elements

--- ForvardList/src/main.cpp
+++ ForvardList/src/main.cpp
@@ -9,19 +9,24 @@ class Element
 {
 	int Data;
 	Element* pNext;
+	 static int count;
 public:
 	Element(int Data, Element* pNext = nullptr) : Data(Data), pNext(pNext)
 	{
+		count++;
 		cout << "EConstructor:\t" << this << endl;
 	}
 
 	~Element()
 	{
+		count--;
 		cout << "EDestructor:\t" << this << endl;
 	}
 
 	friend class ForwardList;
 };
+
+int Element::count = 0;
 
 class ForwardList
 {
@@ -46,6 +51,73 @@ public:
 		New->pNext = Head;
 		Head = New;
 	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr)
+		{
+			return push_front(Data);
+		}
+		Element* New = new Element(Data);
+		Element* Temp = Head;
+		while (Temp->pNext)
+		{
+			Temp = Temp->pNext;
+		}
+		Temp->pNext = New;
+
+	}
+	void insert(int index, int Data)
+	{
+		if (index == 0)
+		{
+			return push_front(Data);
+		}
+		if (index > Head->count)
+		{
+			return;
+		}
+		Element* New = new Element(Data);
+		Element* Temp = Head;
+		for (int i = 0; i < index - 1; i++)
+		{
+			Temp = Temp->pNext;
+		}
+
+		New->pNext = Temp->pNext;
+		Temp->pNext = New;
+
+	}
+
+	//-----------------------Removing elements:----------------------
+	void pop_front()
+	{
+		if (Head == nullptr)
+		{
+			return;
+		}
+		Element* Erased = Head;
+		Head = Head->pNext;
+		delete Erased;
+	}
+	void pop_back()
+	{
+		if (Head == nullptr)
+		{
+			return;
+		}
+		if (Head->pNext == nullptr)
+		{
+			return pop_front();
+		}
+		Element* Temp = Head;
+		while (Temp->pNext->pNext)
+		{
+			Temp = Temp->pNext;
+		}
+		delete Temp->pNext;
+
+		Temp->pNext = nullptr;
+	}
 
 	//------------------------Methods---------------------------------
 
@@ -57,6 +129,7 @@ public:
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;
 		}
+		cout << "Количество элементов списка: " << Head->count << endl;
 	}
 };
 
@@ -71,5 +144,18 @@ void main()
 	{
 		list.push_front(rand() % 100);
 	}
+	list.print();
+	/*list.push_back(123);
+	list.print();
+	list.pop_front();
+	list.pop_back();*/
+	int index;
+	int value;
+	cout << "Введите индек добавляемого элемента: ";
+	cin >> index;
+	cout << "Введите значение добовляемого элемента: ";
+	cin >> value;
+
+	list.insert(index, value);
 	list.print();
 }
